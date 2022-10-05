3c1055f5605f120f38090bc4b2cb3ba11aff19b8
Darcisio Almeida <darcisioalmeida@gmail.com>
2022-10-05 11:16:22 -0300

    Projeto Final e Readme

---
+++ php_poo/Aula 15 - Projeto Final (Parte 2)/Aula15/ProjetoYouTube/Gafanhoto.php
@@ -0,0 +1,34 @@
+<?php
+    require_once 'Pessoa.php';
+    class Gafanhoto extends Pessoa{
+        //Atributos
+            private $login, $totAssistido;
+            
+        //Métodos Específicos
+            public function assistirMaisUm(){
+                $this->totAssistido++;
+            }
+            
+        //Métodos Especiais
+            //Construtor
+                public function __construct($nome, $idade, $sexo, $login) {
+                    parent:: __construct($nome, $idade, $sexo);
+                    $this->login = $login;
+                    $this->totAssistido = 0;
+                }
+                
+            //Getters e Setters
+                public function getLogin() {
+                    return $this->login;
+                }
+                public function getTotAssistido() {
+                    return $this->totAssistido;
+                }
+                public function setLogin($login) {
+                    $this->login = $login;
+                }
+                public function setTotAssistido($totAssistido) {
+                $this->totAssistido = $totAssistido;
+            }
+    }
+?>
